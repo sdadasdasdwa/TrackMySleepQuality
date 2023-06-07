510521a2bb0d54c84798c060b780a0297faddc06
sdadasdasdwa <3433878731@qq.com>
2023-06-07 14:29:49 +0800

    SleepDatabase

--- app/src/main/java/com/example/trackmysleepquality/databse/SleepDatabase.kt
+++ app/src/main/java/com/example/trackmysleepquality/databse/SleepDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.trackmysleepquality.databse
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(version = 1, entities = [SleepNight::class], exportSchema = false)
@@ -10,5 +12,21 @@ abstract class SleepDatabase : RoomDatabase(){
         @Volatile
         private var INSTANCE: SleepDatabase?= null
 
+
+        fun getInstance(context: Context): SleepDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if(instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext,
+                                                    SleepDatabase::class.java,
+                                               "sleep_history_database" )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/trackmysleepquality/databse/SleepNight.kt
+++ app/src/main/java/com/example/trackmysleepquality/databse/SleepNight.kt
@@ -2,20 +2,36 @@ package com.example.trackmysleepquality.databse
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "daily_sleep_quality_table")
 data class SleepNight (
-    @PrimaryKey(autoGenerate=true)
-    var nightId : Long = 0L,
+//    @PrimaryKey(autoGenerate=true)
+//    var nightId : Long = 0L,
+//
+//    @ColumnInfo(name = "start_time_milli")
+//    val startTimeMilli: Long = System.currentTimeMillis(),
+//
+//    @ColumnInfo(name = "end_time_milli")
+//    var endTimeMilli : Long = startTimeMilli,
+//
+//    @ColumnInfo(name = "quality_rating")
+//    var sleepQuality : Int = -1
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
 
     @ColumnInfo(name = "start_time_milli")
     val startTimeMilli: Long = System.currentTimeMillis(),
 
     @ColumnInfo(name = "end_time_milli")
-    var endTimeMilli : Long = startTimeMilli,
+    var endTimeMilli: Long = startTimeMilli,
 
     @ColumnInfo(name = "quality_rating")
-    var sleepQuality : Int = -1
+    var sleepQuality: Int = -1
 
-)
+) {
+    // 添加一个无参数的构造函数，并使用 @Ignore 注解标记
+    @Ignore
+    constructor() : this(0L, System.currentTimeMillis(), System.currentTimeMillis(), -1)
+}
